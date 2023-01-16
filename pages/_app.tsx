2bb0701e29574c4f84cbf809fcd592942519c78d
Max Phillips <max@maxphillips.dev>
2023-01-16 20:16:43 +1100

    basic layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="flex h-12 items-center justify-center border-b-2 border-slate-3 bg-slate-2">
+        <nav>
+          <ul className="flex items-center justify-center space-x-2">
+            <li>Home</li>
+            <li>Projects</li>
+            <li>About</li>
+          </ul>
+        </nav>
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <main>
+      <main className="p-4">
         <p>sheeeesh</p>
       </main>
     </>
